b55668c2fce71b73427e18009b414ea80a48d07f
Biosplat <>
2022-01-20 22:12:33 -0500

    make it cool

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,24 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	// Music
+	{ "", "if [ \"$(playerctl metadata artist)\" ]; then echo \"[  $(playerctl metadata title) ]\"; fi", 2, 0 },
+
+	// Memory
+	{ "", "echo \"[  $(free -h | awk '(NR==2){ print $3 }')B ]\" | sed -r 's/([0-9])+/& /'", 2, 0 },
+	
+	// Volume 
+	{ "", "echo \"[  $(pactl get-sink-volume $(pactl get-default-sink) | awk '(NR==1){ print $5 }') ]\"", 2, 0 },
+
+	// Date
+	{ "", "echo \"[  $(date \"+%b %e\") ]\"", 2, 0 },
+
+	// Time
+	{ "", "echo \"[  $(date \"+%R\") ]\"", 2, 0 }
+
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
-static unsigned int delimLen = 5;
+static char delim[] = " ";
+static unsigned int delimLen = 1;
